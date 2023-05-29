7ef8895fcb0a4fc3d1c7a51b0b95162d320f72b1
onur yucel <onuryucel200@hotmail.com>
2023-05-29 10:37:32 +0300

    update vector 2F

--- vector/MLC_vec2f.c
+++ vector/MLC_vec2f.c
@@ -1,16 +1,67 @@
 #include"MLC_vec2f.h"
 
 
-void MLC_vec2f_initZeroVec(){}
-void MLC_vec2f_init(float x,float y){}
+void MLC_vec2f_initZeroVec(){
+    mlc_vec2f* vec2f = malloc(mlc_vec2f);
 
-void MLC_vec2f_add(vec2f* a,vec2f* b){}
-void MLC_vec2f_sub(vec2f* a,vec2f* b){}
-void MLC_vec2f_mult(vec2f* a,float m){}
-void MLC_vec2f_sub(vec2f* a,float m){}
+    if(vec2f == NULL) return;
 
-float MLC_vec2f_magnitude(vec2f* a){}
-vec2f* MLC_vec2f_getNormalize(vec2f* a){}
-void MLC_vec2f_normalize(){}
+    vec2f->x = 0;
+    vec2f->y = 0;
+}
 
-void MLC_vec2f_destroy(){}
+void MLC_vec2f_init(float x,float y){
+    mlc_vec2f* vec2f = malloc(mlc_vec2f);
+
+    if(vec2f == NULL) return;
+
+    vec2f->x = x;
+    vec2f->y = y;
+}
+
+void MLC_vec2f_add(mlc_vec2f* a,mlc_vec2f* b){
+    a->x = a->x + b->x;
+    a->y = a->y + b->y;
+}
+
+void MLC_vec2f_sub(mlc_vec2f* a,mlc_vec2f* b){
+    a->x = a->x - b->x;
+    a->y = a->y - b->y;
+}
+
+void MLC_vec2f_mult(mlc_vec2f* a,float m){
+    a->x = a->x * m;
+    a->y = a->y * m;
+}
+
+void MLC_vec2f_sub(mlc_vec2f* a,float m){
+    a->x = a->x / m;
+    a->y = a->y / m;
+}
+
+float MLC_vec2f_magnitude(mlc_vec2f* a){
+    return sqrt( (a->x) * (a->x) + (a->y)*(a->y) );
+}
+
+mlc_vec2f* MLC_vec2f_getNormalize(mlc_vec2f* a){
+
+    float magnitude = MLC_vec2f_magnitude(a);
+
+    mlc_vec2f* normalized =  MLC_vec2f_initZeroVec();
+
+    normalized->x = a->x / magnitude;
+    normalized->y = a->y / magnitude;
+
+    return normalized;
+}
+
+void MLC_vec2f_normalize(mlc_vec2f* a){
+    float magnitude = MLC_vec2f_magnitude(a);
+
+    a->x = a->x / magnitude;
+    a->y = a->y / magnitude;
+}
+
+void MLC_vec2f_destroy(mlc_vec2f* a){
+    free(a);
+}
--- vector/MLC_vec2f.h
+++ vector/MLC_vec2f.h
@@ -2,22 +2,28 @@
 #define MLC_VEC_2F_H
 
 #include<stdlib.h>
+#include<math.h>
 
-typedef struct stdMLC_vec2f vec2f;
+typedef struct stdMLC_vec2f mlc_vec2f;
+
+struct stdMLC_vec2f{
+    float x;
+    float y;
+};
 
 void MLC_vec2f_initZeroVec();
 void MLC_vec2f_init(float x,float y);
 
-void MLC_vec2f_add(vec2f* a,vec2f* b);
-void MLC_vec2f_sub(vec2f* a,vec2f* b);
-void MLC_vec2f_mult(vec2f* a,float m);
-void MLC_vec2f_sub(vec2f* a,float m);
+void MLC_vec2f_add(mlc_vec2f* a,mlc_vec2f* b);
+void MLC_vec2f_sub(mlc_vec2f* a,mlc_vec2f* b);
+void MLC_vec2f_mult(mlc_vec2f* a,float m);
+void MLC_vec2f_sub(mlc_vec2f* a,float m);
 
-float MLC_vec2f_magnitude(vec2f* a);
-vec2f* MLC_vec2f_getNormalize(vec2f* a);
-void MLC_vec2f_normalize();
+float MLC_vec2f_magnitude(mlc_vec2f* a);
+mlc_vec2f* MLC_vec2f_getNormalize(mlc_vec2f* a);
+void MLC_vec2f_normalize(mlc_vec2f* a);
 
-void MLC_vec2f_destroy();
+void MLC_vec2f_destroy(mlc_vec2f* a);
 
 #endif // MLC_VEC_2F_H
 
